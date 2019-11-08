a97e2935aacee46b4d3e6b9edc22345a6ae3a07d
Windrider91 <44244404+Windrider91@users.noreply.github.com>
2019-11-08 14:51:03 -0600

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,3 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191015145558) do
-
-  create_table "movies", force: :cascade do |t|
-    t.string   "title"
-    t.string   "rating"
-    t.text     "description"
-    t.datetime "release_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-end
